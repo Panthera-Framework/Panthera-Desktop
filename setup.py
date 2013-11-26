7c75542e36f1b9962548d006bbf9fcde6a836af1
webnull <webnull.www@gmail.com>
2013-11-26 11:26:19 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='pantheradesktop',
+      description = "Panthera Desktop Framework",
+      long_description = "Tiny desktop framework for easy application development in Python using PyQT/PySide",
+      author = "Damian Kęska",
+      author_email = "webnull.www@gmail.com",
+      version=0.1,
+      license = "LGPL",
+      package_dir={'': 'src'},      
+      packages=['pantheradesktop'],
+      data_files = []
+     )
