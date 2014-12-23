1ed0ad876e74801646d16f6ff30ae4b7f9395916
Damian Kęska <damian.keska@fingo.info>
2014-12-23 08:38:56 +0100

    Added Python interactive console and clear console hotkeys in interactive module by default (p and l keys)

--- src/pantheradesktop/interactive.py
+++ src/pantheradesktop/interactive.py
@@ -1,6 +1,11 @@
 from pantheradesktop import pantheraClass
 import pantheradesktop.tools as tools
 import sys
+import os
+from code import InteractiveConsole
+
+class Console(InteractiveConsole):
+    def __init__(*args): InteractiveConsole.__init__(*args)
 
 class pantheraInteractiveConsole(pantheraClass):
     thread = None
@@ -18,7 +23,32 @@ class pantheraInteractiveConsole(pantheraClass):
         """
 
         self.recognizedChars['q'] = self.app.pa_exit
+        self.recognizedChars['l'] = self.clearConsole
+        self.recognizedChars['p'] = self.pythonConsole
         self.thread, self.worker = tools.createThread(self.interactiveConsole)
+
+
+    def clearConsole(self):
+        """
+        Clear console screen
+        :return:
+        """
+
+        os.system('clear')
+
+
+    def pythonConsole(self):
+        """
+        Starts an interactive Python console
+        :return:
+        """
+
+        app = panthera = self.app
+
+        vars = globals().copy()
+        vars.update(locals())
+        console = Console(vars)
+        console.interact()
 
 
     def interactiveConsole(self, thread=''):
